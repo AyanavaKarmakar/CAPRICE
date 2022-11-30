76a73832f545234885e2d8642a53d236f2b61c04
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-12-01 00:14:21 +0530

    better bg gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
         <title>CAPRICE</title>
         <meta property="og:title" content="CAPRICE" key="title_0" />
       </Head>
-      <div className="bg-gradient-to-r from-slate-900/10 via-cyan-900/40 to-zinc-900/10">
+      <div className="bg-gradient-to-r from-indigo-900/10 via-cyan-900/40 to-violet-900/10">
         <ExcelFileUploader />
         <SheetVisualization />
         <footer className="p-5 text-center text-xl tracking-wider">
